52091e97a7642efbc14ada90a4b9de9659c7d722
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-06-08 15:39:08 -0300

    - Changed class name;

--- src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaValoresIdeais.java
+++ src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaValoresIdeais.java
@@ -3,7 +3,7 @@ package edu.utfpr.cp.dacom.sa.soilcorrection;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class NewEmptyJUnitTest {
+public class TestaValoresIdeais {
 
         @Test
         public void testaValoresIdeaisArgiloso() {
