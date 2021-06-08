2614c61c40f8902b126bdae736f1832ee1c79788
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-06-08 14:36:02 -0300

    - Added phosphorus convertions;

---
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/Conversao.java
@@ -0,0 +1,6 @@
+package edu.utfpr.cp.dacom.sa.soilcorrection;
+
+public interface Conversao<T, R> {
+
+    public R converte(T valor);
+}
---
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/ConverteMgDm3EmKgHa.java
@@ -0,0 +1,14 @@
+package edu.utfpr.cp.dacom.sa.soilcorrection;
+
+public class ConverteMgDm3EmKgHa implements Conversao<Double, Double> {
+
+    @Override
+    public Double converte(Double valor) {
+
+        if (valor <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return valor * 2;
+    }
+}
