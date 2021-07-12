184ad26bdbe48d41fe5f218d84f3a000eaa4fc61
jm-marcel <joaomarcel040800@gmail.com>
2021-07-12 18:01:18 -0300

    feat: calculos para correcao da memoria

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/CorrecaoCalcioMagnesio.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/CorrecaoCalcioMagnesio.java
@@ -2,30 +2,118 @@ package edu.utfpr.cp.dacom.sa.soilcorrection;
 
 public class CorrecaoCalcioMagnesio implements ICorrecaoNutriente<FonteCalcioMagnesio> {
 
+    // (Memória de Cálculo) Método de Cálculo de Teor de CaO a Adicionar: F96
     public double teorCaO(
         double calcio,
         double fosfatagem,
         double participacaoCTCExistente,
         double participacaoCTCDesejada
     ) {
-        return ((calcio * participacaoCTCDesejada)
-                    / participacaoCTCExistente
-            ) - (calcio - fosfatagem);
+        if (calcio <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (fosfatagem <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (participacaoCTCExistente <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (participacaoCTCDesejada <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return ((calcio * participacaoCTCDesejada) / participacaoCTCExistente) - (calcio - fosfatagem);
     }
 
+    // (Memória de Cálculo) Método de Quantidade de Corretivo a Incorporar: I113
+    public double calculaQuantidadeCorretivo(
+        double quantidade,
+        double prnt
+    ) {
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (prnt <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return (quantidade * 100) / prnt;
+    }
+
+    // (Memória de Cálculo) Método de Quantidade Total de Corretivo a Incorporar: F117/G117
+    public double calculaQuantidadeTotalCorretivo(
+        double quantidade,
+        double ha
+    ) {
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (ha <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return quantidade * ha;
+    }
+
+    // Método Pós-Correção
     public double calculaValorDeCorrecaoCalcio(
         double calcio,
         double potassio,
         double magnesio,
+        double hAl,
+        double fosfatagem,
         double participacaoCTCExistente,
         double participacaoCTCDesejada
     ) {
-            if (participacaoCTCExistente <= 0) {
-                throw new IllegalArgumentException();
-            }
+        if (calcio <= 0) {
+            throw new IllegalArgumentException();
+        }
 
-            if (participacaoCTCDesejada <= 0) {
-                throw new IllegalArgumentException();
-            }
+        if (potassio <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (magnesio <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (hAl <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (fosfatagem <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (participacaoCTCExistente <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (participacaoCTCDesejada <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        // Cálculo de Cmol/Dm3
+        double cmolDm3 = calcio + potassio + magnesio; // 7.54
+        // Cálculo Al Total Pela Tabela
+        double aluminio = cmolDm3 + hAl; // 12.89
+        // Cálculo de Quantidade de Teor de CaO
+        double teorCaO = teorCaO(calcio, fosfatagem, participacaoCTCExistente, participacaoCTCDesejada); // 1.31024832
+        // CaO Total
+        double caOTotal = teorCaO + calcio; // 7.07024832
+
+        return (caOTotal / aluminio) * 100;
+
     }
+
+    // public void validaValor(double valor) {
+    //     if (valor <= 0) {
+    //         throw new IllegalArgumentException();
+    //     }
+    // }
 }
--- src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TesteCorrecaoCalcioMagnesio.java
+++ src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TesteCorrecaoCalcioMagnesio.java
@@ -8,34 +8,74 @@ public class TestaCorrecaoCalcioMagnesio {
 
     @Test
     public void testeDeTeorCaO() {
+        // Instanciando classe CorrecaoCalcioMagnesio
         var correcaoCalcioMagnesio = new CorrecaoCalcioMagnesio();
-        
-        // Exemplo 1 resulta em 1.32724832, já os exemplos 2 e 3 resultam em 0???
 
+        // Equilíbrio e Correção na CTC: E51
         double participacaoCTCDesejada = 55.0;
+        // Equilíbrio e Correção na CTC: E48
         double participacaoCTCExistente = 44.7;
 
+        // Equilíbrio e Correção na CTC: F11/G11
         double calcio = 5.76;
-        double fosfatagem = 0.004;
+        // Memória de Cálculo: I105
+        double fosfatagem = 0.017;
 
-        double teorCaOEsperado = 1.32324832;
+        assertEquals(1.31024832,
+                correcaoCalcioMagnesio.teorCaO(calcio, fosfatagem, participacaoCTCExistente, participacaoCTCDesejada));
+    }
+    
+    @Test
+    public void testeDeQuantidadeCorretivo() {
+        // Instanciando classe CorrecaoCalcioMagnesio
+        var correcaoCalcioMagnesio = new CorrecaoCalcioMagnesio();
 
-        assertEquals(teorCaOEsperado, correcaoCalcioMagnesio.teorCaO(calcio, fosfatagem, participacaoCTCExistente, participacaoCTCDesejada));
+        // Memória de Cálculo: F113/G113
+        double quantidade = 1.29;
+        // Equilíbrio e Correção na CTC: C53
+        double prnt = 70.0;
+
+        assertEquals(1.85, correcaoCalcioMagnesio.calculaQuantidadeCorretivo(quantidade, prnt));
+    }
+
+    @Test
+    public void testeDeQuantidadeTotalCorretivo() {
+        // Instanciando classe CorrecaoCalcioMagnesio
+        var correcaoCalcioMagnesio = new CorrecaoCalcioMagnesio();
+
+        // Memória de Cálculo: F113/G113
+        double quantidade = 1.29;
+        // Equilíbrio e Correção na CTC: H5
+        double ha = 70.0;
+
+        assertEquals(6,2436, correcaoCalcioMagnesio.calculaQuantidadeTotalCorretivo(quantidade, ha));
     }
 
     @Test
     public void testePosCorrecao() {
+        // Instanciando classe CorrecaoCalcioMagnesio
         var correcaoCalcioMagnesio = new CorrecaoCalcioMagnesio();
 
+        // Equilíbrio e Correção na CTC: E51
         double participacaoCTCDesejada = 55.0;
+        // Equilíbrio e Correção na CTC: E48
         double participacaoCTCExistente = 44.7;
 
+        // Equilíbrio e Correção na CTC: F11/G11
         double calcio = 5.76;
+        // Equilíbrio e Correção na CTC: D11/E11
         double potassio = 0.15;
+        // Equilíbrio e Correção na CTC: H11/I11
         double magnesio = 1.63;
+        // Equilíbrio e Correção na CTC: L11
+        double hAl = 5.35;
+        // Memória de Cálculo: I105
+        double fosfatagem = 0.017;
 
-        assertEquals(participacaoCTCDesejada, correcaoCalcioMagnesio.calculaValorDeCorrecaoCalcio(calcio, potassio,
-                magnesio, participacaoCTCExistente, participacaoCTCDesejada));
+        // Memória de Cálculo
+
+        assertEquals(54.850646, correcaoCalcioMagnesio.calculaValorDeCorrecaoCalcio(calcio, potassio,
+                magnesio, hAl, fosfatagem, participacaoCTCExistente, participacaoCTCDesejada));
     }
     
 }
