5f7b5d67cf6e63859b23af19893c54626fdfc344
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-11-18 21:28:37 -0300

    Criando a primeira rota da api e realizando a instalação de uma dependência faltante

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/Main.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/Main.java
@@ -5,5 +5,7 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8080);
+
+        app.get("/", ctx -> ctx.result("home"));
     }
 }
