6f7971c16291744b2917993ee8717a8c8b97f06a
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-11-30 12:10:14 -0300

    Controller para correção de Potássio

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/Main.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/Main.java
@@ -1,11 +1,15 @@
 package edu.utfpr.cp.dacom.sa.soilcorrection;
 
+import edu.utfpr.cp.dacom.sa.soilcorrection.controllers.CalcioMagnesioController;
 import edu.utfpr.cp.dacom.sa.soilcorrection.controllers.FosforoController;
+import edu.utfpr.cp.dacom.sa.soilcorrection.controllers.PotassioController;
 import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8080);
         app.get("/fosforo", FosforoController::calcularCorrecao);
+        app.get("/potassio", PotassioController::calcularQuantidadeAAplicar);
+        app.get("/calcio_magnesio", CalcioMagnesioController::calcularCorrecao);
     }
 }
---
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/controllers/PotassioController.java
@@ -0,0 +1,62 @@
+package edu.utfpr.cp.dacom.sa.soilcorrection.controllers;
+
+import edu.utfpr.cp.dacom.sa.soilcorrection.services.ConverteCMolcDm3EmMgDm3;
+import edu.utfpr.cp.dacom.sa.soilcorrection.services.ConverteKgHaEmK2O;
+import edu.utfpr.cp.dacom.sa.soilcorrection.services.CorrecaoPotassio;
+import edu.utfpr.cp.dacom.sa.soilcorrection.services.FontePotassio;
+import io.javalin.http.Context;
+
+public class PotassioController {
+    private PotassioController() {
+    }
+
+    public static void calcularQuantidadeAAplicar(Context context) {
+        String teorSolorFromQuery = context.queryParam("teor_solo");
+        String fontePotassioFromQuery = context.queryParam("fonte_potassio");
+        String participacaoCTCExistenteFromQuery = context.queryParam("participacao_ctc_existente");
+        String participacaoCTCDesejadaFromQuery = context.queryParam("participacao_ctc_desejada");
+
+        double teorSolo = Double.parseDouble(teorSolorFromQuery);
+        double partCTCExistente = Double.parseDouble(participacaoCTCExistenteFromQuery);
+        double partCTCDesejada = Double.parseDouble(participacaoCTCDesejadaFromQuery);
+        Integer fontePotassio = Integer.parseInt(fontePotassioFromQuery);
+        CorrecaoPotassio correcaoPotassio = new CorrecaoPotassio();
+
+        double necessidadeAdicionarDM3 = correcaoPotassio.calculaNecessidadeAdicionarCMolcDm3(teorSolo, partCTCExistente, partCTCDesejada);
+        double necessidadeAdicionarEmMgDm3 = new ConverteCMolcDm3EmMgDm3().converte(necessidadeAdicionarDM3);
+        double necessidadeAdicionarEmKgHa = necessidadeAdicionarEmMgDm3 * 2;
+        double necessidadeAdicionarEmK20 = new ConverteKgHaEmK2O().converte(necessidadeAdicionarEmKgHa);
+        double eficienciaNutriente = correcaoPotassio.calculaEficienciaNutriente(necessidadeAdicionarEmK20, 0.85);
+
+        FontePotassio teorFonte;
+
+        switch (fontePotassio) {
+            case 1:
+                teorFonte = FontePotassio.CLORETO_POTASSIO;
+                break;
+            case 2:
+                teorFonte = FontePotassio.SULFATO_POTASSIO;
+                break;
+            case 3:
+                teorFonte = FontePotassio.SULFATO_POTASSIO_MAGNESIO;
+                break;
+            default: teorFonte = FontePotassio.CLORETO_POTASSIO;
+                break;
+        }
+        double quantidadeAplicar = correcaoPotassio.calculaQuantidadeAplicar(eficienciaNutriente, teorFonte);
+
+        String jsonResponse = "{" +
+                "necessidade_adicionar_CMolcDm3: " + necessidadeAdicionarDM3 +
+                "," +
+                "necessidade_adicionar_MgDm3: " + necessidadeAdicionarEmMgDm3 +
+                "," +
+                "necessidade_adicionar_KgHa: " + necessidadeAdicionarEmKgHa
+                + "," +
+                "eficiencia_nutriente: " + eficienciaNutriente +
+                "," +
+                "quantidade_aplicar: "+ quantidadeAplicar +
+                "}";
+
+        context.json(jsonResponse);
+    }
+}
